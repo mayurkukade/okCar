027001e2d294bea2b048688c9c9962bf7919a69d
mayurkukade <mayurkukade@opticalarc.com>
2023-06-13 13:13:22 +0530

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react'
-
+import manifest from './manifest.json'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest,
+      devOptions: {
+        enabled: false,
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html}', '**/*.{svg,png,jpg,gif}'],
+      },
+
+    })
+    
+  ]
+ 
+});
