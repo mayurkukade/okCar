3033e01249dc754ef223a09adcebb2839381dde6
mayurkukade <mayurkukade@opticalarc.com>
2023-06-30 18:15:28 +0530

    add

---
+++ src/components/EditDealerProfile/EditDealerProfile.jsx
@@ -0,0 +1,174 @@
+import SubNav from "../Navbar/SubNav";
+import { useState } from "react";
+
+const EditDealerProfile = () => {
+  const [inputField, setInputField] = useState({
+    email: "",
+    mobileNo: "",
+    address: "",
+    city: "",
+    firstName: "",
+    lastName: "",
+
+    adharShopact: "",
+    area: "",
+    shopName: "",
+  });
+
+  const onChangeFormhandler = (e) => {
+    const { name, value } = e.target;
+    setInputField((preVal) => {
+      return {
+        ...preVal,
+        [name]: value,
+      };
+    });
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(inputField);
+  };
+  return (
+    <>
+      <SubNav componentsName={"Edit Dealer"} />
+      <div className="listpgWraper" style={{ backgroundColor: "#F5F7F9" }}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-md-offset-3">
+              <div className="userccount">
+                <form onSubmit={onSubmitHandler}>
+                  <div className="userbtns">
+                    <h2>Edit Dealer Details </h2>
+                  </div>
+                  <div className="tab-content">
+                    <div
+                      id="wsell"
+                      className="formpanel tab-pane fade in active"
+                    >
+                      <div className="formrow">
+                        <input
+                          type="text"
+                          name="firstName"
+                          className="form-control"
+                          placeholder="First Name"
+                          onChange={onChangeFormhandler}
+                          required
+                        />
+                      </div>
+                      <div className="formrow">
+                        <input
+                          type="text"
+                          name="lastName"
+                          className="form-control"
+                          placeholder="Last Name"
+                          onChange={onChangeFormhandler}
+                          required
+                        />
+                      </div>
+                      <div className="formrow">
+                        <input
+                          type="text"
+                          name="mobileNo"
+                          className="form-control"
+                          placeholder="Phone Number"
+                          onChange={onChangeFormhandler}
+                          required
+                        />
+                      </div>
+                      <div className="formrow">
+                        <input
+                          type="text"
+                          name="email"
+                          className="form-control"
+                          placeholder="Email"
+                          onChange={onChangeFormhandler}
+                          required
+                        />
+                      </div>
+                      <div className="formrow">
+                        <input
+                          type="text"
+                          name="address"
+                          className="form-control"
+                          placeholder="Address"
+                          onChange={onChangeFormhandler}
+                          required
+                        />
+                      </div>
+                      <div className="formrow">
+                        <input
+                          type="text"
+                          name="city"
+                          className="form-control"
+                          placeholder="City"
+                          onChange={onChangeFormhandler}
+                          required
+                        />
+                      </div>
+                      <div className="formrow">
+                        <input
+                          type="text"
+                          name="adharShopact"
+                          className="form-control"
+                          placeholder="Adhaar Card / Shop Cart"
+                          onChange={onChangeFormhandler}
+                          required
+                        />
+                      </div>
+                      <div className="formrow">
+                        <input
+                          type="text"
+                          name="area"
+                          className="form-control"
+                          placeholder="Area"
+                          onChange={onChangeFormhandler}
+                          required
+                        />
+                      </div>
+                      <div className="formrow">
+                        <input
+                          type="text"
+                          name="shopName"
+                          className="form-control"
+                          placeholder="Shop Name"
+                          onChange={onChangeFormhandler}
+                          required
+                        />
+                      </div>
+                      <div className="formrow">
+                        <input
+                          type="checkbox"
+                          value="agree text"
+                          name="agree"
+                          style={{ marginRight: "10px" }}
+                          required
+                        />
+                        Terms and Condition
+                      </div>
+                      <div style={{ display: "flex", gap: "10px" }}>
+                        <button type="submit" className="btn" value="Register">
+                          Edit User
+                        </button>
+                        <button
+                          type="submit"
+                          className="btn"
+                          value="Register"
+                          style={{ backgroundColor: "red" }}
+                        >
+                          Delete User
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EditDealerProfile;
---
+++ src/components/EditUserProfile/EditUserProfile.jsx
@@ -0,0 +1,148 @@
+import SubNav from "../Navbar/SubNav";
+import { useState } from "react";
+
+const EditUserProfile = () => {
+  const [inputField, setInputField] = useState({
+    firstName: "",
+    lastName: "",
+    mobile_no: "",
+    email: "",
+    address: "",
+    city: "",
+  });
+
+  const onChangeFormhandler = (e) => {
+    const { name, value } = e.target;
+    setInputField((preVal) => {
+      return {
+        ...preVal,
+        [name]: value,
+      };
+    });
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(inputField);
+  };
+  return (
+    <>
+      <SubNav componentsName={"Edit User"} />
+      <div className="listpgWraper" style={{ backgroundColor: "#F5F7F9" }}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-md-offset-3">
+              <div className="userccount">
+                <form onSubmit={onSubmitHandler}>
+                  <div className="userbtns">
+                    <h2>Edit User Details </h2>
+                  </div>
+                  <div className="tab-content">
+                    <div
+                      id="wsell"
+                      className="formpanel tab-pane fade in active"
+                    >
+                      <div className="formrow">
+                        <input
+                          type="text"
+                          name="firstName"
+                          className="form-control"
+                          placeholder="First Name"
+                          onChange={onChangeFormhandler}
+                          required
+                        />
+                      </div>
+                      <div className="formrow">
+                        <input
+                          type="text"
+                          name="lastName"
+                          className="form-control"
+                          placeholder="Last Name"
+                          onChange={onChangeFormhandler}
+                          required
+                        />
+                      </div>
+                      <div className="formrow">
+                        <input
+                          type="text"
+                          name="mobile_no"
+                          className="form-control"
+                          placeholder="Phone Number"
+                          onChange={onChangeFormhandler}
+                          required
+                        />
+                      </div>
+                      <div className="formrow">
+                        <input
+                          type="text"
+                          name="email"
+                          className="form-control"
+                          placeholder="Email"
+                          onChange={onChangeFormhandler}
+                          required
+                        />
+                      </div>
+                      <div className="formrow">
+                        <input
+                          type="text"
+                          name="address"
+                          className="form-control"
+                          placeholder="Address"
+                          onChange={onChangeFormhandler}
+                          required
+                        />
+                      </div>
+                      <div className="formrow">
+                        <input
+                          type="text"
+                          name="city"
+                          className="form-control"
+                          placeholder="City"
+                          onChange={onChangeFormhandler}
+                          required
+                        />
+                      </div>
+                      <div className="formrow">
+                        <input
+                          type="checkbox"
+                          value="agree text"
+                          name="agree"
+                          style={{ marginRight: "10px" }}
+                          required
+                        />
+                        Terms and Condition
+                      </div>
+                      <div style={{ display: "flex", gap: "10px" }}>
+                        <button type="submit" className="btn" value="Register">
+                          Edit User
+                        </button>
+                        <button
+                          type="submit"
+                          className="btn"
+                          value="Register"
+                          style={{ backgroundColor: "red" }}
+                        >
+                          Delete User
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* <div className="newuser">
+                      <i className="fa fa-user" aria-hidden="true"></i> Already a
+                      Member?
+                      <Link to="/signin">
+                        <a> Login Here</a>
+                      </Link>
+                    </div> */}
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EditUserProfile;
