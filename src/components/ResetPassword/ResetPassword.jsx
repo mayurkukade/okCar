766799e08f511dd86dd44122e2663b504569383b
Aksh-007 <akshaybarapatre54@gmail.com>
2023-07-06 16:18:43 +0530

    Feat - Added Reset Password page

--- src/components/ResetPassword/ResetPassword.jsx
+++ src/components/ResetPassword/ResetPassword.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import SubNav from "../Navbar/SubNav";
+import { useToast } from "@chakra-ui/react";
+import { useResetPasswordMutation } from "../../api/usersApiSlice";
 
 const ChangePasswordPage = () => {
+  const toast = useToast();
   const navigate = useNavigate();
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const token = searchParams.get("token");
+
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [error, setError] = useState("");
+
+  const { mutate: resetPassword } = useResetPasswordMutation();
 
   const handleNewPasswordChange = (e) => {
     setNewPassword(e.target.value);
@@ -15,63 +24,84 @@ const ChangePasswordPage = () => {
     setConfirmPassword(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (newPassword === confirmPassword) {
-      console.log("Password updated successfully");
-      navigate("/");
-    } else {
-      setError("Passwords do not match");
+    try {
+      if (newPassword === confirmPassword) {
+        console.log(token, newPassword);
+        const response = await resetPassword({ password: newPassword, token });
+        if (response.error) {
+          toast({
+            status: "error",
+            position: "top",
+            description: response.error.message,
+          });
+        } else {
+          toast({
+            status: "success",
+            position: "top",
+            description: "Password updated successfully",
+          });
+          navigate("/");
+        }
+      } else {
+        toast({
+          status: "error",
+          position: "top",
+          description: "Password and Confirm Password do not match",
+        });
+      }
+    } catch (error) {
+      toast({
+        status: "error",
+        position: "top",
+        description: "An error occurred while updating the password",
+      });
     }
   };
 
   return (
-    <div
-      className="listpgWraper"
-      // style={{
-      //   height: "80vh",
-      //   display: "flex",
-      //   justifyContent: "center",
-      //   alignItems: "center",
-      // }}
-    >
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6 col-md-offset-3">
-            <div className="userccount">
-              <h5>Reset Password</h5>
-              <div className="formpanel">
-                <form onSubmit={handleSubmit}>
-                  <div className="formrow">
-                    <input
-                      type="password"
-                      className="form-control"
-                      placeholder="New Password"
-                      value={newPassword}
-                      onChange={handleNewPasswordChange}
-                      required
-                    />
-                  </div>
-                  <div className="formrow">
-                    <input
-                      type="password"
-                      className="form-control"
-                      placeholder="Confirm Password"
-                      value={confirmPassword}
-                      onChange={handleConfirmPasswordChange}
-                      required
-                    />
-                  </div>
-                  {error && <p>{error}</p>}
-                  <input type="submit" className="btn" value="Update" />
-                </form>
+    <>
+      <SubNav componentsName={"Reset Password"} />
+      <div className="listpgWraper">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-md-offset-3">
+              <div className="userccount">
+                <h5>Reset Password</h5>
+                <p>Here is the token: {token}</p>
+                <div className="formpanel">
+                  <form onSubmit={handleSubmit}>
+                    <div className="formrow">
+                      <input
+                        type="password"
+                        className="form-control"
+                        placeholder="New Password"
+                        value={newPassword}
+                        onChange={handleNewPasswordChange}
+                        required
+                      />
+                    </div>
+                    <div className="formrow">
+                      <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Confirm Password"
+                        value={confirmPassword}
+                        onChange={handleConfirmPasswordChange}
+                        required
+                      />
+                    </div>
+                    <input type="submit" className="btn" value="Update" />
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -114,7 +114,7 @@ const SignIn = () => {
                 <div className="newuser">
                   <i className="fa fa-user" aria-hidden="true"></i> New User?{" "}
                   <Link to="/signup">Register Here</Link> |
-                  <Link to="/resetpassword"> Forgot Password</Link>
+                  <Link to="/ForgotPassword"> Forgot Password</Link>
                 </div>
               </div>
             </div>
