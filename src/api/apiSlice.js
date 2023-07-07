823b7cf5ba03b17f6c0d545c540d450850b73f7d
devharpopticalarc <harpreetsinghwasrikar@opticalarc.com>
2023-07-07 15:04:15 +0530

    hotfix

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react'
 
-const baseQuery = fetchBaseQuery({baseUrl:'https://crazy-geckos-hang.loca.lt'})
+const baseQuery = fetchBaseQuery({baseUrl:' https://chilly-items-feel.loca.lt'})
 // const baseQuery = fetchBaseQuery({baseUrl:'http://localhost:8088'},)
 
 
