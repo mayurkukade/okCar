a20e0c34d31c30ab0818c95e88c1bed6c317de7a
devharpopticalarc <harpreetsinghwasrikar@opticalarc.com>
2023-07-07 10:54:12 +0530

    hotfix: global be api

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react'
 
-const baseQuery = fetchBaseQuery({baseUrl:'https://bumpy-snails-wear.loca.lt'})
+const baseQuery = fetchBaseQuery({baseUrl:'https://crazy-geckos-hang.loca.lt/'})
 
 export const apiSlice = createApi({
     baseQuery,
