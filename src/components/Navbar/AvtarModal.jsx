1b4b342536be1dc93d8fddf58857e6d86effc25a
Aksh-007 <akshaybarapatre54@gmail.com>
2023-07-10 19:43:47 +0530

    Feat - editUserDetails added

--- src/components/Navbar/AvtarModal.jsx
+++ src/components/Navbar/AvtarModal.jsx
@@ -15,8 +15,7 @@ import { logout } from "../../api/authSlice";
 const AvtarModal = () => {
   const username = localStorage.getItem("userInfo");
   //  console.log(JSON.parse(username)?.results)
-  console.log(JSON.parse(username))
-   
+  console.log(JSON.parse(username));
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -34,14 +33,18 @@ const AvtarModal = () => {
     <Box className="card">
       <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
       <Menu bg="white">
-        <MenuButton as={Button} rightIcon={<ChevronDownIcon />} style={{ marginLeft: "10px", padding: "23px" }}>
+        <MenuButton
+          as={Button}
+          rightIcon={<ChevronDownIcon />}
+          style={{ marginLeft: "10px", padding: "23px" }}
+        >
           {JSON.parse(username)?.firstname}
         </MenuButton>
         <MenuList>
           {/* <MenuItem>History</MenuItem>
           <MenuItem>FAQ Managment</MenuItem>
           <MenuItem>Settings</MenuItem> */}
-          <Link to="/edit">
+          <Link to="/edituserdetails">
             <MenuItem color={"black"}>Edit Profile</MenuItem>
           </Link>
           <MenuItem onClick={logoutHandler}>Logout</MenuItem>
@@ -52,4 +55,3 @@ const AvtarModal = () => {
 };
 
 export default AvtarModal;
-
