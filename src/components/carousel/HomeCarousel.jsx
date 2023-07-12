e72ff10e0b4a0bb87f1c879191d749be0e4b8469
Aksh-007 <akshaybarapatre54@gmail.com>
2023-07-12 19:05:21 +0530

    Fix - UI issue fix

--- src/components/carousel/HomeCarousel.jsx
+++ src/components/carousel/HomeCarousel.jsx
@@ -1,30 +1,32 @@
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper";
-import carslider from "../../../images/slider/cs1.png"
+import carslider from "../../../images/slider/cs1.png";
 // import carslider2 from "../../../images/slider/cs2.png"
-import carslider3 from "../../../images/slider/cs3.png"
-import carslider4 from "../../../images/slider/cs4.png"
-import './homecarousel.css'
+import carslider3 from "../../../images/slider/cs3.png";
+import carslider4 from "../../../images/slider/cs4.png";
+import "./homecarousel.css";
 const HomeCarousel = () => {
   return (
     <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper" style={{ width: '100%', height: '400px', margin: '0 auto' }} // Adjust the width and height values as per your requirement
->
+      <Swiper
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
+        style={{ width: "100%", height: "650px", margin: "0 auto" }} // Adjust the width and height values as per your requirement
+      >
         <SwiperSlide>
-    
-            <img src={carslider}  style={{ width: '100%', height: '150%' }}/>
-             </SwiperSlide>
+          <img src={carslider} style={{ width: "100%", height: "100%" }} />
+        </SwiperSlide>
         {/* <SwiperSlide>
         <img src={carslider2} style={{ width: '100%', height: '150%' }}/>  
         </SwiperSlide> */}
         <SwiperSlide>
-        <img src={carslider3}  style={{ width: '100%', height: '150%' }} />
+          <img src={carslider3} style={{ width: "100%", height: "100%" }} />
         </SwiperSlide>
         <SwiperSlide>
-        <img src={carslider4} style={{ width: '100%', height: '150%' }}/>
+          <img src={carslider4} style={{ width: "100%", height: "100%" }} />
         </SwiperSlide>
         {/* <SwiperSlide> <img src={carslider} /></SwiperSlide>
         <SwiperSlide> <img src={carslider2} /></SwiperSlide>
@@ -33,10 +35,7 @@ const HomeCarousel = () => {
         <SwiperSlide> <img src={carslider} /></SwiperSlide> */}
       </Swiper>
     </>
-      
-    
-  )
-}
+  );
+};
 
-export default HomeCarousel
-
+export default HomeCarousel;
