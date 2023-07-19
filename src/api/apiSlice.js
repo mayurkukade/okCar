2154ee4eb1e470d6c06266aa6de2ecf17e562aaa
Aksh-007 <akshaybarapatre54@gmail.com>
2023-07-19 11:57:49 +0530

    Feat - Navbar Scroll effect added

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react'
 
-export const baseUrl = 'https://d243-144-48-178-178.ngrok-free.app';
+export const baseUrl = 'https://0a88-144-48-178-178.ngrok-free.app';
 export const baseQuery = fetchBaseQuery({ baseUrl })
 
 export const apiSlice = createApi({
     baseQuery,
-    tagTypes: ['User', "Admin", "Dealer"],
+    tagTypes: ["User", "Admin", "Dealer"],
     endpoints: (builder) => ({})
 })
